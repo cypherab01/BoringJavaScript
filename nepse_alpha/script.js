a105442edcd6504d877a7f84e36fc9151e1b2cfe
Abhishek Ghimire <aghimire074@gmail.com>
2023-12-07 13:09:26 +0545

    Some changes

--- nepse_alpha/script.js
+++ nepse_alpha/script.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Nepse Alpha
-// @namespace    http://tampermonkey.net/
-// @version      1.1
+// @namespace    https://github.com/cypherab01
+// @version      1.2
 // @description  Remove boring dialogue box at starup
 // @author       Abhishek Ghimire (@cypherab01)
 // @match        https://nepsealpha.com/trading/chart
 // @updateURL    https://raw.githubusercontent.com/cypherab01/BoringJavaScript/main/nepse_alpha/script.js
 // @downloadURL  https://raw.githubusercontent.com/cypherab01/BoringJavaScript/main/nepse_alpha/script.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=nepsealpha.com
+// @homepage     https://github.com/cypherab01/BoringJavaScript/tree/main/nepse_alpha
 // @grant        none
 // ==/UserScript==
 
